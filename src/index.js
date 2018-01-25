19070cb73f6c9c064b7263747208a5239e8196ec
Pedro Gomes <pedrogomes94@gmail.com>
2018-01-25 22:04:56 +0000

    updated

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { injectGlobal } from 'styled-components';
+import { globalStyles } from './styles';
+import App from './App';
 
-ReactDOM.render(<div />, document.getElementById('root'));
+// eslint-disable-next-line
+injectGlobal`${globalStyles}`;
+
+ReactDOM.render(<App />, document.getElementById('root'));
